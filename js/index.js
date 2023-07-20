b098e543cadf5d86df1db0b94a094ff3b094cd49
ItsSebis <sebi@sebis.net>
2023-07-20 14:21:03 +0200

    1.1

--- js/index.js
+++ js/index.js
@@ -5,24 +5,24 @@ let gender = true
 let ACs = {
     girl: {
         sprint: {
-            50: {a: 3.64800, c: 0.00660, name: "50m"},
-            75: {a: 3.99800, c: 0.00660, name: "75m"},
-            100: {a: 4.00620, c: 0.00656, name: "100m"}
+            50: {a: 3.64800, c: 0.00660, name: "50m", id: "sprint1"},
+            75: {a: 3.99800, c: 0.00660, name: "75m", id: "sprint2"},
+            100: {a: 4.00620, c: 0.00656, name: "100m", id: "sprint3"}
         },
         run: {
-            800: {a: 2.02320, c: 0.00647, name: "800m"},
-            2000: {a: 1.80000, c: 0.00540, name: "2000m"},
-            3000: {a: 1.75000, c: 0.00500, name: "3000m"}
+            800: {a: 2.02320, c: 0.00647, name: "800m", id: "run1"},
+            2000: {a: 1.80000, c: 0.00540, name: "2000m", id: "run2"},
+            3000: {a: 1.75000, c: 0.00500, name: "3000m", id: "run3"}
         },
         jump: {
-            highjump: {a: 0.88070, c: 0.00068, name: "Hochsprung"},
-            longjump: {a: 1.09350, c: 0.00208, name: "Weitsprung"}
+            highjump: {a: 0.88070, c: 0.00068, name: "Hochsprung", id: "jump1"},
+            longjump: {a: 1.09350, c: 0.00208, name: "Weitsprung", id: "jump2"}
         },
         ball: {
-            shotput: {a: 1.27900, c: 0.00398, name: "Kugelstoßen"},
-            slingshot: {a: 1.08500, c: 0.00921, name: "Schleuderball"},
-            Ball200G: {a: 1.41490, c: 0.01039, name: "200g"},
-            Ball80G: {a: 2.02320, c: 0.00874, name: "80g"}
+            shotput: {a: 1.27900, c: 0.00398, name: "Kugelstoßen", id: "ball3"},
+            slingshot: {a: 1.08500, c: 0.00921, name: "Schleuderball", id: "ball4"},
+            Ball200G: {a: 1.41490, c: 0.01039, name: "200g", id: "ball2"},
+            Ball80G: {a: 2.02320, c: 0.00874, name: "80g", id: "ball1"}
         },
         certificates: {
             8: {s: 475, e: 625},
@@ -41,24 +41,24 @@ let ACs = {
     },
     boy: {
         sprint: {
-            50: {a: 3.79000, c: 0.00690, name: "50m"},
-            75: {a: 4.10000, c: 0.00664, name: "75m"},
-            100: {a: 4.34100, c: 0.00676, name: "100m"}
+            50: {a: 3.79000, c: 0.00690, name: "50m", id: "sprint1"},
+            75: {a: 4.10000, c: 0.00664, name: "75m", id: "sprint2"},
+            100: {a: 4.34100, c: 0.00676, name: "100m", id: "sprint3"}
         },
         run: {
-            1000: {a: 2.15800, c: 0.00600, name: "1000m"},
-            2000: {a: 1.78400, c: 0.00600, name: "2000m"},
-            3000: {a: 1.70000, c: 0.00580, name: "3000m"}
+            1000: {a: 2.15800, c: 0.00600, name: "1000m", id: "run1"},
+            2000: {a: 1.78400, c: 0.00600, name: "2000m", id: "run2"},
+            3000: {a: 1.70000, c: 0.00580, name: "3000m", id: "run3"}
         },
         jump: {
-            highjump: {a: 0.84100, c: 0.00080, name: "Hochsprung"},
-            longjump: {a: 1.15028, c: 0.00219, name: "Weitsprung"}
+            highjump: {a: 0.84100, c: 0.00080, name: "Hochsprung", id: "jump1"},
+            longjump: {a: 1.15028, c: 0.00219, name: "Weitsprung", id: "jump2"}
         },
         ball: {
-            shotput: {a: 1.42500, c: 0.00370, name: "Kugelstoßen"},
-            slingshot: {a: 1.59500, c: 0.009125, name: "Schleuderball"},
-            Ball200G: {a: 1.93600, c: 0.01240, name: "200g"},
-            Ball80G: {a: 2.80000, c: 0.01100, name: "80g"}
+            shotput: {a: 1.42500, c: 0.00370, name: "Kugelstoßen", id: "ball3"},
+            slingshot: {a: 1.59500, c: 0.009125, name: "Schleuderball", id: "ball4"},
+            Ball200G: {a: 1.93600, c: 0.01240, name: "200g", id: "ball2"},
+            Ball80G: {a: 2.80000, c: 0.01100, name: "80g", id: "ball1"}
         },
         certificates: {
             8: {s: 450, e: 575},
@@ -89,11 +89,11 @@ function genderSwap() {
     if (!gender) {
         gString = "Jungen"
         gColor = "blue"
-        document.getElementById("gRun").href = "javascript:runPoints(Number(document.getElementById('runMin').value*60)+Number(document.getElementById('runSec').value), 1000)"
-        document.getElementById("gRun").innerText = "1000m"
+        document.getElementById("run1").href = "javascript:runPoints(Number(document.getElementById('runMin').value*60)+Number(document.getElementById('runSec').value), 1000)"
+        document.getElementById("run1").innerText = "1000m"
     } else {
-        document.getElementById("gRun").href = "javascript:runPoints(Number(document.getElementById('runMin').value*60)+Number(document.getElementById('runSec').value), 800)"
-        document.getElementById("gRun").innerText = "800m"
+        document.getElementById("run1").href = "javascript:runPoints(Number(document.getElementById('runMin').value*60)+Number(document.getElementById('runSec').value), 800)"
+        document.getElementById("run1").innerText = "800m"
     }
     document.getElementById("gender").innerText = gString
     document.getElementById("gender").style.color = gColor
@@ -159,19 +159,29 @@ function sprintPoints(sec, dist) {
     if (points <= 0 || points >= 1000) {
         return;
     }
+
+    let sprintId = ACs.girl.sprint[dist].id
+    console.log(sprintId)
+    for (let discipline of document.getElementsByClassName("sprint")[0].getElementsByTagName("a")) {
+        discipline.classList.remove("active")
+    }
+    document.getElementById(sprintId).classList.add("active")
+
     document.getElementById("sprintPoints").innerText = String(points)
     addPoints();
 }
 
 function runPoints(sec, dist) {
     let points = 0
-    let a,c
+    let a,c,runId
     if (gender) {
         a = ACs.girl.run[dist].a;
         c = ACs.girl.run[dist].c;
+        runId = ACs.girl.run[dist].id
     } else {
         a = ACs.boy.run[dist].a;
         c = ACs.boy.run[dist].c;
+        runId = ACs.boy.run[dist].id
     }
     sec = Math.abs(sec);
     points = Math.round((dist / sec - a) / c);
@@ -179,6 +189,13 @@ function runPoints(sec, dist) {
     if (points <= 0 || points >= 1000) {
         return;
     }
+
+    console.log(runId)
+    for (let discipline of document.getElementsByClassName("run")[0].getElementsByTagName("a")) {
+        discipline.classList.remove("active")
+    }
+    document.getElementById(runId).classList.add("active")
+
     document.getElementById("runPoints").innerText = String(points)
     addPoints();
 }
@@ -193,12 +210,20 @@ function jumpPoints(dist, type) {
         a = ACs.boy.jump[type].a;
         c = ACs.boy.jump[type].c;
     }
-    dist = Math.abs(dist)/100;
+    dist = Math.abs(dist);
     points = Math.round((Math.sqrt(dist) - a) / c);
     console.log(points + " " + dist + " " + type)
     if (points <= 0 || points >= 1000) {
         return;
     }
+
+    let jumpId = ACs.girl.jump[type].id
+    console.log(jumpId)
+    for (let discipline of document.getElementsByClassName("jump")[0].getElementsByTagName("a")) {
+        discipline.classList.remove("active")
+    }
+    document.getElementById(jumpId).classList.add("active")
+
     document.getElementById("jumpPoints").innerText = String(points)
     addPoints();
 }
@@ -218,6 +243,14 @@ function ballPoints(dist, type) {
     if (points <= 0 || points >= 1000) {
         return;
     }
+
+    let ballId = ACs.girl.ball[type].id
+    console.log(ballId)
+    for (let discipline of document.getElementsByClassName("ball")[0].getElementsByTagName("a")) {
+        discipline.classList.remove("active")
+    }
+    document.getElementById(ballId).classList.add("active")
+
     document.getElementById("ballPoints").innerText = String(points)
     addPoints();
 }
